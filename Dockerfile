5e65daec6f806a79ebc97a064556e4c20b3f90bb
justinengland <justin@chia.net>
2021-03-13 00:39:04 -0700

    more logic at startup

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM ubuntu:latest
 
-ARG plot_dir
-ARG keys
-ARG harvester
-ARG farmer
+env plot_dir
+env keys
+env harvester
+env farmer
+env plots_dir
+env farmer_address
+env farmer_port
 
 RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y curl jq python3 ansible tar bash ca-certificates git openssl unzip wget python3-pip sudo acl build-essential python3-dev python3.8-venv python3.8-distutils apt nfs-common python-is-python3
 
@@ -12,11 +15,12 @@ RUN git clone https://github.com/Chia-Network/chia-blockchain.git \
 && chmod +x install.sh \
 && /usr/bin/sh ./install.sh
 
+WORKDIR /chia-blockchain
+
 ADD ./chiarun chiarun
 ADD ./entrypoint.sh entrypoint.sh
 
-WORKDIR /chia-blockchain
-USER ubuntu
+
 
 
 ENTRYPOINT ["bash", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,35 @@ cd /chia-blockchain
 
 chia init
 
-tail -f /dev/null
+if [[ -z ${keys} ]]; then
+  echo "please set your keys or set the keys variable equal to generate"
+  exit
+elif [[ ${keys} == "generate" ]]; then
+  chia keys generate
+else
+  chia keys add -m "${keys}"
+fi
+
+if [[ ! "$(ls -A /plots)" && -z ${plots_dir} ]]; then
+  echo "Plots directory appears to be empty and you have not specified another, try mounting a plot directory with the docker -v command "
+elif [[ ! -z ${plots_dir} ]]; then
+  chia plots add -d ${plots_dir}
+else
+  chia plots add -d /plots
+fi
+
+if [[ ${farmer} == 'true' ]]; then
+  chia start farmer-only
+elif [[ ${harvester} == 'true' ]]; then
+  if [[ -z ${farmer_address} || -z ${farmer_port} ]]; then
+    echo "A farmer peer address and port are required."
+    exit
+  else
+    chia configure --set-farmer-peer ${farmer_address}:${farmer_port}
+    chia start harvester
+  fi
+else
+  chia start farmer
+fi
+  #statements
+while true; do sleep 30; done;
