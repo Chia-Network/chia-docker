cd9ed5dfa9980adba7649bc7123342acab3ed78d
Zachary Brown <z.brown@chia.net>
2022-03-02 14:07:24 -0800

    Add language to README to make the use of keys more clear.  Update entrypoint script with similar language in messaging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,7 +21,7 @@ fi
 if [[ ${keys} == "persistent" ]]; then
   echo "Not touching key directories"
 elif [[ ${keys} == "generate" ]]; then
-  echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
+  echo "to use your own keys pass the mnemonic as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
   chia keys generate
 elif [[ ${keys} == "copy" ]]; then
   if [[ -z ${ca} ]]; then
