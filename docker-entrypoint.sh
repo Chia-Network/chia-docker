44ed08171e802263897b535369215db6bb4e63e5
stonemelody <90731692+stonemelody@users.noreply.github.com>
2022-07-19 21:09:19 -0700

    Create non-root build target
    Running `docker build --target no-root` will build a container image
    without sudo and as the user/group `chia`. UID and GID can be adjusted
    with the UID and GID build-arg parameters.
    
    Gotchas:
      * if volumes are mounted in the container, they must have open read/write
        permissions if the host uid:gid for them is different than the
        container's or the must have the same uid:gid as the container
      * if the volumes are mounted but the intervening directories in the
        mount path inside the container do not exist, docker will create those
        directories as root:root by default, this will break permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC2154
-if [[ -n "${TZ}" ]]; then
-  echo "Setting timezone to ${TZ}"
-  ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" > /etc/timezone
-fi
-
 cd /chia-blockchain || exit 1
 
 # shellcheck disable=SC1091
