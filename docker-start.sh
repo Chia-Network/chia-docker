64ca78e2db18df4e8bfc370d3e3fb66163b6f228
Carles Moreno <carlesm@outlook.com>
2023-01-08 18:51:11 -0800

    Add ENV variable for custom chia arguments
    For example to be able to set --passphrase-file, without which chia will not start or start corrupting an existing blockchain db

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC2154,SC2086
-chia start ${service}
+chia ${chia_args} start ${service}
 
 trap "echo Shutting down ...; chia stop all -d; exit 0" SIGINT SIGTERM
 
